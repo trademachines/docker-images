a0634414f92bb826512ec84486814713df524189
anho <me@anho.github.com>
2016-05-24 21:47:02 +0200

    be able to build specific versions

--- build-all.sh
+++ build-all.sh
@@ -2,8 +2,24 @@
 
 set -ev
 
+function build_push {
+  if [ -f "$2/Dockerfile" ]; then
+    docker build -t "$1" "$2"
+    docker push "$1"
+  fi
+}
+
 for dir in $(ls -d */); do
   name=trademachines/${dir%%/}
-  docker build -t "$name" ${dir}
-  docker push "$name"
+
+  # build latest image
+  build_push "${name}" "${dir}"
+
+  # build specific tags
+  if ls -d ${dir}*/ 1> /dev/null 2>&1; then
+    for tag in $(ls -d ${dir}*/); do
+      version="$name:$(echo ${tag%%/} | cut -d '/' -f 2)"
+      build_push "${version}" "${tag}"
+    done
+  fi
 done
