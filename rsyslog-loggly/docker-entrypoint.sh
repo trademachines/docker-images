9b25428f619202e5b5e87110aaba7fcb62442f47
anho <me@anho.github.com>
2016-09-26 16:16:20 +0200

    add image to push logs to loggly

---
+++ rsyslog-loggly/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+# handle files
+file_conf=/etc/rsyslog.d/files.conf
+
+for k in $LOGGLY_FILES; do
+  logfile="${k}.log"
+
+  echo "\$InputFileName /var/log/$logfile" >> $file_conf
+  echo "\$InputFileTag ${k}" >> $file_conf
+  echo "\$InputFileStateFile state-$logfile" >> $file_conf
+  echo "\$InputFileFacility local6" >> $file_conf
+  echo "\$InputRunFileMonitor" >> $file_conf
+  echo >> $file_conf
+done
+
+# handle common tags
+tags=''
+for t in $LOGGLY_TAGS; do
+  tags="$tags tag=\\\\\"${t}\\\\\""
+done
+
+echo $tags
+
+sed -i "s/LOGGLY_TOKEN/$LOGGLY_TOKEN/g" /etc/rsyslog.d/loggly.conf
+sed -i "s/ LOGGLY_TAGS/$tags/g" /etc/rsyslog.d/loggly.conf
+
+rsyslogd -n
