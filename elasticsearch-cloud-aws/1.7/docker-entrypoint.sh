534571ad2668de9839a28f81663f8aa7c56e8198
anho <me@anho.github.com>
2017-12-07 14:26:49 +0100

    add optional way of determining image is running on ec2

--- elasticsearch-cloud-aws/1.7/docker-entrypoint.sh
+++ elasticsearch-cloud-aws/1.7/docker-entrypoint.sh
@@ -2,7 +2,17 @@
 
 set -e
 
-if [[ -f /sys/hypervisor/uuid ]] && [[ `head -c 3 /sys/hypervisor/uuid` == ec2 ]]; then
+function hasEc2Prefix() {
+  local file=$1
+
+  if [[ -f $file ]] && [[ `head -c 3 $file | awk '{print tolower($0)}'` == ec2 ]]; then
+    echo "1"
+  else
+    echo "0"
+  fi
+}
+
+if [[ `hasEc2Prefix /sys/hypervisor/uuid` == 1 ]] || [[ `hasEc2Prefix /sys/devices/virtual/dmi/id/product_uuid` == 1 ]]; then
   instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
   region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c 2- | rev)
   lifecycle=$(aws ec2 describe-instances --region $region --instance-ids $instance_id --query 'Reservations[0].Instances[0].InstanceLifecycle' --output text)
